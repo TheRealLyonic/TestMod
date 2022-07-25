965096daeada4c04925533bba2a6a3856d618404
TheRealLyonic <rwbyfan2305@gmail.com>
2022-07-24 22:14:11 -0400

    Added ToolTip to JumpyBlock

--- src/main/java/com/lyonic/testmod/block/custom/JumpyBlock.java
+++ src/main/java/com/lyonic/testmod/block/custom/JumpyBlock.java
@@ -1,5 +1,7 @@
 package com.lyonic.testmod.block.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
@@ -9,10 +11,16 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class JumpyBlock extends Block {
 
@@ -28,6 +36,18 @@ public class JumpyBlock extends Block {
         }
 
         return super.use(blockState, level, blockPos, player, hand, hitResult);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable BlockGetter blockGetter, List<Component> components, TooltipFlag tooltipFlag) {
+        if(Screen.hasShiftDown()){
+            components.add(Component.literal("Release SHIFT for less info.").withStyle(ChatFormatting.YELLOW));
+            components.add(Component.literal("Step on to gain jump boost.").withStyle(ChatFormatting.AQUA));
+        }else{
+            components.add(Component.literal("Press SHIFT for more info.").withStyle(ChatFormatting.YELLOW));
+        }
+
+        super.appendHoverText(itemStack, blockGetter, components, tooltipFlag);
     }
 
     @Override
